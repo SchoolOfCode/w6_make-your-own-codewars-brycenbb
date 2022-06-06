54561f812d125a8a64d6b5cf0780c34e69135ec3
brycen <brycensoc@gmail.com>
2022-06-06 10:59:46 +0100

    MVP

--- main.test.js
+++ main.test.js
@@ -1,1 +1,46 @@
 //👉 Write your tests below here:
+const objectArraySum = require('./main.js');
+
+test('Tests for valid input', () => {
+  //not giving an array
+  expect(objectArraySum(1)).toBe(0);
+  //not giving an array of objects
+  expect(objectArraySum([1, 2, 3])).toBe(0);
+  //not giving an array of objects with arrays
+  expect(
+    objectArraySum([
+      { boolean: true, numbers: 2 },
+      { boolean: true, numbers: 5 },
+    ])
+  ).toBe(0);
+  //not giving an array of objects with arrays of numbers
+  expect(
+    objectArraySum([
+      { boolean: true, numbers: ['a', 'b'] },
+      { boolean: true, numbers: [true, false] },
+    ])
+  ).toBe(0);
+  //not giving any true booleans
+  expect(
+    objectArraySum([
+      { boolean: false, numbers: [2] },
+      { boolean: false, numbers: [1, 2] },
+    ])
+  ).toBe(0);
+});
+
+test('Tests for correct sums', () => {
+  //Expect a non zero sum for correct input
+  expect(
+    objectArraySum([
+      { boolean: true, numbers: [2] },
+      { boolean: true, numbers: [1, 2] },
+    ])
+  ).toBe(3);
+  expect(
+    objectArraySum([
+      { boolean: true, numbers: [2] },
+      { boolean: false, numbers: [1, 2] },
+    ])
+  ).toBe(2);
+});
